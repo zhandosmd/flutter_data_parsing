b391970d726f5d5a44d45afe91c2aa493a22ff3e
zhandosmd <64863365+zhandosmd@users.noreply.github.com>
2021-10-12 18:32:12 +0600

    Update api_client.dart

--- lib/domain/api_clients/api_client.dart
+++ lib/domain/api_clients/api_client.dart
@@ -10,7 +10,7 @@ class ApiClient{
   Future<List<Post>> getPosts() async{
     final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
     // same as  Uri(scheme: 'https', host: 'jsonplaceholder.typicode.com', path: 'posts');
-    final request = await client.getUrl(url); // will return FUTURE, await will ждать пока реквест собереться
+    final request = await client.getUrl(url); // will return FUTURE, await will ждать пока реквест соберутся
     final response = await request.close(); // отправили на сервер и ждем респонс
     final jsonStrings = await response.transform(utf8.decoder).toList(); // converting from Stream List<Byte> to String List
     final jsonString = jsonStrings.join() as List<dynamic>;
